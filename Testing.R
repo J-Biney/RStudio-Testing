9c562f93c5f71303aef2425e1fa6a6846dd3c426
J.B <bineyjosiah1@gmail.com>
2025-07-02 10:12:34 +0200

    Mistakes updated III

--- Testing.R
+++ Testing.R
@@ -1,3 +1,6 @@
-print("this file was breated within RStudio")
+print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+print("Now I want to try this myself to see how it works")
+print("It was a nice first project")
+print("Why is it not working")
